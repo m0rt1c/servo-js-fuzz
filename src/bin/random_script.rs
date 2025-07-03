a7d778134be71aa5ac2751105dc84d5eed829532
Andrea Jegher <andreajegher@radicallyopensecurity.com>
2025-07-03 15:16:37 +0200

    Adds two new targets

---
+++ src/bin/random_script.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate afl;
+extern crate servo;
+
+use std::sync::OnceLock;
+
+use servo_js_fuzz::{ServoTest, run_script_on};
+thread_local! {
+  static SERVO_LOCK: OnceLock<ServoTest> = OnceLock::new();
+}
+
+fn main() {
+    fuzz!(|data: &[u8]| {
+        if let Ok(input_data) = std::str::from_utf8(data) {
+            SERVO_LOCK.with(|cell| {
+                let servo_test = cell.get_or_init(|| ServoTest::new());
+                let _ = run_script_on(servo_test, &input_data);
+            });
+        }
+    });
+}
---
+++ src/bin/readable_stream.rs
@@ -0,0 +1,38 @@
+#[macro_use]
+extern crate afl;
+extern crate servo;
+
+use std::sync::OnceLock;
+
+use servo_js_fuzz::{ServoTest, run_script_on};
+thread_local! {
+  static SERVO_LOCK: OnceLock<ServoTest> = OnceLock::new();
+}
+
+const SCRIPT_FORMAT: &str = r#"
+async function target(input) {
+    const readableStream = new ReadableStream(
+        {
+            start(controller) {
+            controller.enqueue(new TextEncoder().encode(input));
+            controller.close();
+            }
+        }
+    );
+    for await (const chunk of readableStream) {
+    }
+}
+target("%input%)")
+"#;
+
+fn main() {
+    fuzz!(|data: &[u8]| {
+        if let Ok(input_data) = std::str::from_utf8(data) {
+            SERVO_LOCK.with(|cell| {
+                let servo_test = cell.get_or_init(|| ServoTest::new());
+                let script = SCRIPT_FORMAT.replace("%input%", input_data);
+                let _ = run_script_on(servo_test, &script);
+            });
+        }
+    });
+}
