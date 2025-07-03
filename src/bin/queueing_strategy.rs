10a156157b9218a3bfedf6c810076ba921a7ed05
Andrea Jegher <andreajegher@radicallyopensecurity.com>
2025-07-03 17:31:55 +0200

    Fixes access to data

--- src/bin/queueing_strategy.rs
+++ src/bin/queueing_strategy.rs
@@ -33,11 +33,13 @@ target(%input%);
 
 fn main() {
     fuzz!(|data: &[u8]| {
-        let input_data = u64::from_le_bytes(data.try_into().expect("Wrong length"));
-        SERVO_LOCK.with(|cell| {
-            let servo_test = cell.get_or_init(|| ServoTest::new());
-            let script = SCRIPT_FORMAT.replace("%input%", &format!("{:?}", input_data));
-            let _ = run_script_on(servo_test, &script);
-        });
+        if let Ok(number) = data.try_into() {
+            let input_data = u64::from_le_bytes(number);
+            SERVO_LOCK.with(|cell| {
+                let servo_test = cell.get_or_init(|| ServoTest::new());
+                let script = SCRIPT_FORMAT.replace("%input%", &format!("{:?}", input_data));
+                let _ = run_script_on(servo_test, &script);
+            });
+        }
     });
 }
