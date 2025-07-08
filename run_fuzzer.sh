fa4bfcad89c05f68d65b5d52c34eb715c2501909
Andrea Jegher <andreajegher@radicallyopensecurity.com>
2025-07-08 11:28:54 +0200

    Update run fuzzer to fuzz all targets

--- run_fuzzer.sh
+++ run_fuzzer.sh
@@ -2,79 +2,33 @@
 
 set -euo pipefail
 
-SESSION_NAME=$(basename $1)
-TARGET=$1
-INPUT_DIR=$2
-OUTPUT_DIR=$3
-CORES=$4
-
 export AFL_TESTCACHE_SIZE=200
 export AFL_IMPORT_FIRST=1
 export AFL_FINAL_SYNC=1
 export AFL_IGNORE_SEED_PROBLEMS=1
 export AFL_NO_AFFINITY=1
 
-tmux kill-session -t "$SESSION_NAME" 2>/dev/null || true
-tmux new-session -d -s "$SESSION_NAME" -n main
+INPUT_DIR="./in"
 
-# --- Main INSTANCE (LAF-INTEL + persistent) ---
-tmux send-keys -t "$SESSION_NAME:main" \
-	"AFL_LLVM_LAF_ALL=1 AFL_LLVM_INSTRUMENT=PCGUARD cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -M main-${SESSION_NAME} -p fast $TARGET" C-m
+for TARGET in $(find ./target/debug -maxdepth 1 -type f -executable);
+do
+    SESSION_NAME=$(basename $TARGET)
+    OUTPUT_DIR="./out/$SESSION_NAME"
 
-# --- SECONDARY INSTANCES ---
-for i in $(seq 0 8); do
-    WIN="sec-$i"
-    CMD_PREFIX=""
-    SCHED=""
-    ARGS=""
-    ENV_VARS=""
+    tmux kill-session -t "$SESSION_NAME" 2>/dev/null || true
 
-    case $i in
-        0)
-            CMD_PREFIX="AFL_USE_ASAN=1 AFL_USE_UBSAN=1"
-            SCHED="-p exploit"
-            ;;
-        1)
-            CMD_PREFIX="AFL_LLVM_CMPLOG=1"
-            SCHED="-p explore"
-            ;;
-        2)
-            CMD_PREFIX="AFL_LLVM_LAF_ALL=1"
-            SCHED="-p coe"
-            ;;
-        3)
-            ARGS="-L 0"
-            SCHED="-p quad"
-            ;;
-        4)
-            ARGS="-Z"
-            SCHED="-p lin"
-            ;;
-        5)
-            ENV_VARS="AFL_DISABLE_TRIM=1"
-            SCHED="-p rare"
-            ;;
-        6)
-            ENV_VARS="AFL_DISABLE_TRIM=1"
-            SCHED="-p explore"
-            ;;
-        7)
-            ENV_VARS="AFL_DISABLE_TRIM=1"
-            ARGS="-a ascii"
-            SCHED="-p exploit"
-            ;;
-        8)
-            ARGS="-a binary"
-            SCHED="-p fast"
-            ;;
-    esac
+    # --- Main instance
+    tmux new-session -d -s "$SESSION_NAME" -n "main"
+    tmux send-keys -t "$SESSION_NAME:main" \
+        "AFL_LLVM_LAF_ALL=1 AFL_LLVM_INSTRUMENT=PCGUARD AFL_FINAL_SYNC=1 cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -M main-${SESSION_NAME} -p fast $TARGET" C-m
 
-    tmux new-window -t "$SESSION_NAME" -n "$WIN"
+    # --- Secondary instances ---
+
+    tmux new-window -t "$SESSION_NAME" -n "secondary #0"
     tmux send-keys -t "$SESSION_NAME:$WIN" \
-        "$ENV_VARS $CMD_PREFIX cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S sec-$SESSION_NAME-$i $ARGS $SCHED $TARGET" C-m
+        "AFL_USE_ASAN=1 AFL_USE_UBSAN=1 AFL_USE_CFISAN=1 cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S sec-${SESSION_NAME}-1 $TARGET" C-m
+
+    tmux new-window -t "$SESSION_NAME" -n "secondary #1"
+    tmux send-keys -t "$SESSION_NAME:$WIN" \
+        "cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S sec-${SESSION_NAME}-2 -l 2AT $TARGET" C-m
 done
-
-# Attach to tmux
-tmux select-window -t "$SESSION_NAME:main"
-tmux attach-session -t "$SESSION_NAME"
-
