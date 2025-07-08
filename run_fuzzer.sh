893fc50c641455baa467b23caa4364bded12e816
Andrea Jegher <andreajegher@radicallyopensecurity.com>
2025-07-08 11:30:49 +0200

    Change fun_fuzzer to accept an argument from commad line

--- run_fuzzer.sh
+++ run_fuzzer.sh
@@ -8,27 +8,24 @@ export AFL_FINAL_SYNC=1
 export AFL_IGNORE_SEED_PROBLEMS=1
 export AFL_NO_AFFINITY=1
 
+$TARGET=$1
 INPUT_DIR="./in"
+SESSION_NAME=$(basename $TARGET)
+OUTPUT_DIR="./out/$SESSION_NAME"
 
-for TARGET in $(find ./target/debug -maxdepth 1 -type f -executable);
-do
-    SESSION_NAME=$(basename $TARGET)
-    OUTPUT_DIR="./out/$SESSION_NAME"
+tmux kill-session -t "$SESSION_NAME" 2>/dev/null || true
 
-    tmux kill-session -t "$SESSION_NAME" 2>/dev/null || true
+# --- Main instance
+tmux new-session -d -s "$SESSION_NAME" -n "main"
+tmux send-keys -t "$SESSION_NAME:main" \
+    "AFL_LLVM_LAF_ALL=1 AFL_LLVM_INSTRUMENT=PCGUARD AFL_FINAL_SYNC=1 cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -M main-${SESSION_NAME} -p fast $TARGET" C-m
 
-    # --- Main instance
-    tmux new-session -d -s "$SESSION_NAME" -n "main"
-    tmux send-keys -t "$SESSION_NAME:main" \
-        "AFL_LLVM_LAF_ALL=1 AFL_LLVM_INSTRUMENT=PCGUARD AFL_FINAL_SYNC=1 cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -M main-${SESSION_NAME} -p fast $TARGET" C-m
+# --- Secondary instances ---
 
-    # --- Secondary instances ---
+tmux new-window -t "$SESSION_NAME" -n "secondary #0"
+tmux send-keys -t "$SESSION_NAME:$WIN" \
+    "AFL_USE_ASAN=1 AFL_USE_UBSAN=1 AFL_USE_CFISAN=1 cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S sec-${SESSION_NAME}-1 $TARGET" C-m
 
-    tmux new-window -t "$SESSION_NAME" -n "secondary #0"
-    tmux send-keys -t "$SESSION_NAME:$WIN" \
-        "AFL_USE_ASAN=1 AFL_USE_UBSAN=1 AFL_USE_CFISAN=1 cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S sec-${SESSION_NAME}-1 $TARGET" C-m
-
-    tmux new-window -t "$SESSION_NAME" -n "secondary #1"
-    tmux send-keys -t "$SESSION_NAME:$WIN" \
-        "cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S sec-${SESSION_NAME}-2 -l 2AT $TARGET" C-m
-done
+tmux new-window -t "$SESSION_NAME" -n "secondary #1"
+tmux send-keys -t "$SESSION_NAME:$WIN" \
+    "cargo afl fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S sec-${SESSION_NAME}-2 -l 2AT $TARGET" C-m
