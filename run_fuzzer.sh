13ccda35ac5742ce8bdbff7d7874b1ddfcc0f3cb
Andrea Jegher <andreajegher@radicallyopensecurity.com>
2025-07-09 11:37:27 +0200

    Improves script

--- run_fuzzer.sh
+++ run_fuzzer.sh
@@ -2,13 +2,14 @@
 
 set -x -euo pipefail
 
-if [[ $# -lt 1 ]]; then
-  echo "Usage: $0 <target-binary>"
+if [[ $# -lt 2 ]]; then
+  echo "Usage: $0 <target-binary> <input-dir-path> [true]"
+  echo "Set true to add two secondary instances, instead of just one main instance"
   exit 1
 fi
 
 TARGET="$1"
-INPUT_DIR="${INPUT_DIR:-./in}"
+INPUT_DIR="$2"
 SESSION_NAME=$(basename "$TARGET")
 OUTPUT_DIR="./out/$SESSION_NAME"
 
@@ -27,16 +28,18 @@ tmux new-session -d -s "$SESSION_NAME" -n "main"
 tmux send-keys -t "$SESSION_NAME:main" \
   "AFL_LLVM_LAF_ALL=1 AFL_LLVM_INSTRUMENT=PCGUARD cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -M main-${SESSION_NAME} \"$TARGET\"" C-m
 
-SECONDARY="$2"
+if [[ $# -ge 3 ]]; then
+    SECONDARY="$3"
 
-if [ "$SECONDARY" == "true" ]; then
-    # --- Secondary instance 1 ---
-    tmux new-window -t "$SESSION_NAME" -n "sec1"
-    tmux send-keys -t "$SESSION_NAME:sec1" \
-    "AFL_USE_ASAN=1 AFL_USE_UBSAN=1 AFL_USE_CFISAN=1 cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -S sec-${SESSION_NAME}-1 \"$TARGET\"" C-m
+    if [ "$SECONDARY" == "true" ]; then
+        # --- Secondary instance 1 ---
+        tmux new-window -t "$SESSION_NAME" -n "sec1"
+        tmux send-keys -t "$SESSION_NAME:sec1" \
+        "AFL_USE_ASAN=1 AFL_USE_UBSAN=1 AFL_USE_CFISAN=1 cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -S sec-${SESSION_NAME}-1 \"$TARGET\"" C-m
 
-    # --- Secondary instance 2 ---
-    tmux new-window -t "$SESSION_NAME" -n "sec2"
-    tmux send-keys -t "$SESSION_NAME:sec2" \
-    "cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -S sec-${SESSION_NAME}-2 -l 2AT \"$TARGET\"" C-m
+        # --- Secondary instance 2 ---
+        tmux new-window -t "$SESSION_NAME" -n "sec2"
+        tmux send-keys -t "$SESSION_NAME:sec2" \
+        "cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -S sec-${SESSION_NAME}-2 -l 2AT \"$TARGET\"" C-m
+    fi
 fi
