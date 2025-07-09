5344c960bc628539082375538e562a65fe52d7bc
Andrea Jegher <andreajegher@radicallyopensecurity.com>
2025-07-09 11:18:35 +0200

    Adds secondary option

--- run_fuzzer.sh
+++ run_fuzzer.sh
@@ -25,14 +25,18 @@ tmux kill-session -t "$SESSION_NAME" 2>/dev/null || true
 # --- Main instance ---
 tmux new-session -d -s "$SESSION_NAME" -n "main"
 tmux send-keys -t "$SESSION_NAME:main" \
-  "AFL_LLVM_LAF_ALL=1 AFL_LLVM_INSTRUMENT=PCGUARD cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -M main-${SESSION_NAME} -p fast \"$TARGET\"" C-m
+  "AFL_LLVM_LAF_ALL=1 AFL_LLVM_INSTRUMENT=PCGUARD cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -M main-${SESSION_NAME} \"$TARGET\"" C-m
 
-# --- Secondary instance 1 ---
-tmux new-window -t "$SESSION_NAME" -n "sec1"
-tmux send-keys -t "$SESSION_NAME:sec1" \
-  "AFL_USE_ASAN=1 AFL_USE_UBSAN=1 AFL_USE_CFISAN=1 cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -S sec-${SESSION_NAME}-1 \"$TARGET\"" C-m
+SECONDARY="$2"
 
-# --- Secondary instance 2 ---
-tmux new-window -t "$SESSION_NAME" -n "sec2"
-tmux send-keys -t "$SESSION_NAME:sec2" \
-  "cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -S sec-${SESSION_NAME}-2 -l 2AT \"$TARGET\"" C-m
+if [ "$SECONDARY" == "true" ]; then
+    # --- Secondary instance 1 ---
+    tmux new-window -t "$SESSION_NAME" -n "sec1"
+    tmux send-keys -t "$SESSION_NAME:sec1" \
+    "AFL_USE_ASAN=1 AFL_USE_UBSAN=1 AFL_USE_CFISAN=1 cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -S sec-${SESSION_NAME}-1 \"$TARGET\"" C-m
+
+    # --- Secondary instance 2 ---
+    tmux new-window -t "$SESSION_NAME" -n "sec2"
+    tmux send-keys -t "$SESSION_NAME:sec2" \
+    "cargo afl fuzz -i \"$INPUT_DIR\" -o \"$OUTPUT_DIR\" -S sec-${SESSION_NAME}-2 -l 2AT \"$TARGET\"" C-m
+fi
