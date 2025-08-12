aa98482327612c831da6f836622d4a8e038b59a4
jegher <andrea.jegher@pm.me>
2025-08-12 11:28:13 +0200

    Adds filter crashes script

---
+++ triage/filter_crashes.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Copy all the crashes in one single folder
+
+find ./out -type f -path '*/crashes/*' -not -name '*.txt' -exec cp {} ./triage/crashes \;
+
+# Use afl tmin to minimize the crash corpus
+
+find ./triage/crashes -type f -not -name '.gitkeep' -print -exec bash -c 'cargo afl tmin -i {} -o ./triage/crashes-min/{}.min.js -- ./target/debug/eval_script' \;
+
+# Use afl cmin to find unique crashes
+
+cargo afl tmin -C -i ./triage/crashes-min/ -o ../triage/crashes-unique/ ./target/debug/eval_script
+
+
